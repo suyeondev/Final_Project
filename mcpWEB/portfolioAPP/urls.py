4c822cf15cc5e7987815cc81300eb8a37ec6b698
suyeon <suyeon97dev@gmail.com>
2021-11-12 20:05:59 +0900

    웹 화면 구성 수정2

--- mcpWEB/portfolioAPP/urls.py
+++ mcpWEB/portfolioAPP/urls.py
@@ -3,5 +3,5 @@ from portfolioAPP import views
 
 urlpatterns = [
     path('main/',views.index, name='main'),
-    path('table/',views.table,name='table'),
+    path('main/songinfo/',views.songInfo,name='songinfo'),
 ]
--- mcpWEB/portfolioAPP/views.py
+++ mcpWEB/portfolioAPP/views.py
@@ -6,6 +6,6 @@ def index(request):
     print('portfolioAPP index~~')
     return render(request, 'portfolio/dashboard.html')
 
-def table(request):
-    print('portfolioAPP table')
-    return render(request, 'portfolio/tables.html')
+def songInfo(request):
+    print('portfolioAPP songinfo~~')
+    return render(request, 'portfolio/songinfo.html')
