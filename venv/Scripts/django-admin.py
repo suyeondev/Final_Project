8f1e80a34fa7908ca4028d31716e760f0b4b9903
hongjin11 <koreahongjin@gmail.com>
2021-11-17 09:51:25 +0900

    first commit

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\HongYeeun\PycharmProjects\pythonProject\Final_Project\venv\Scripts\python.exe
+#!C:\Users\vghdw\PycharmProjects\Final_Project\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
