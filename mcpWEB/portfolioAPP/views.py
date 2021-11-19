a11f58cb558a55b899dbe5a19db81272d84be0e8
suyeon <suyeon97dev@gmail.com>
2021-11-19 18:04:25 +0900

    데이터 넘겨받는 것까지 완료

--- mcpWEB/portfolioAPP/views.py
+++ mcpWEB/portfolioAPP/views.py
@@ -13,25 +13,61 @@ def index(request):
         propensity= request.POST.get('propensity')
         money = request.POST.get('money')
         musics = SongInfo.objects.all()
+        per = [0.2,0.15,0.1,0.1,0.1,0.05,0.05]
+
+        la_ss_per=[0.25,0.2,0.15,0.1,0.1,0.1,0.05,0.05]
+        seed = []
+        for i in per:
+            seed_money = round(i * float(money))
+            seed.append(seed_money)
+
+        la_ss_seed = []
+        for l in la_ss_per:
+            lll = round(l * float(money))
+            la_ss_seed.append(lll)
+        context = {}
+        short_safe=[]
+        short_agg = []
+        long_safe =[]
+        long_agg =[]
+        la_ss_per_txt=[25,20,15,10,10,10,5,5]
+        per_txt = [20, 15, 10, 10, 10, 5, 5]
+        first_seed = round(0.25 * float(money))
+        last_seed =  round(0.05 * float(money))
+        print('period : {}, propensity: {}, money : {}'.format(period, propensity, money))
+        if (period == "short") & (propensity == "sta"):
+            short_safe = SongInfo.objects.filter(Q(Q(cluster_bs=0) | Q(cluster_bs=1))&Q(price__lte = money)).order_by('-fee_near_year','-price')[:8]
+
+        if (period == "short") & (propensity == "agg"):
+            short_agg = SongInfo.objects.filter(Q(cluster_bs=2)&Q(price__lte = money)).order_by('price','-fee_near_year')[:7]
+
+        if (period == "long") & (propensity == "sta"):
+            long_safe = SongInfo.objects.filter(Q(Q(cluster_bl=0) | Q(cluster_bl=1))&Q(price__lte = money)).order_by('-fee_near_year','-price')[:7]
+
+        if (period == "long") & (propensity == "agg"):
+            long_agg = SongInfo.objects.filter(Q(cluster_bl=2)&Q(price__lte = money)).order_by('price','-fee_near_year')[:8]
+
+        ss_zip = zip(short_safe,la_ss_per, la_ss_seed,la_ss_per_txt)
+        sa_zip = zip(short_agg, per, seed,per_txt)
+        ls_zip = zip(long_safe, per, seed,per_txt)
+        la_zip = zip(long_agg, la_ss_per, la_ss_seed, la_ss_per_txt)
+
         context = {
-            'musics': musics,
             'period': period,
             'propensity': propensity,
             'money': money,
+            'ss_zip': ss_zip,
+            'sa_zip': sa_zip,
+            'la_zip': la_zip,
+            'ls_zip': ls_zip,
+            'musics': musics,
+            'first_seed':first_seed,
+            'last_seed':last_seed,
         }
-        print('period : {}, propensity: {}, money : {}'.format(period, propensity, money))
-        if (period == "short") & (propensity == "sta"):
-            context['short_safe'] = SongInfo.objects.filter(Q(cluster_bs=0)|Q(cluster_bs=1)).order_by('-fee_near_year','-price')
-
-        if (period == "short") & (propensity == "agg"):
-            context['short_agg'] = SongInfo.objects.filter(Q(cluster_bs=2)).order_by('price','-fee_near_year')
-
-        if (period == "long") & (propensity == "sta"):
-            context['long_safe'] = SongInfo.objects.filter(Q(cluster_bl=0) | Q(cluster_bl=1)).order_by('-fee_near_year','-price')
-
-        if (period == "long") & (propensity == "agg"):
-            context['long_agg'] = SongInfo.objects.filter(Q(cluster_bl=2)).order_by('price','-fee_near_year')
-
+        # for ls, p, s in loop_zip :
+        #     print(ls.title)
+        #     print(p)
+        #     print(s)
     return render(request, 'portfolio/dashboard.html',context)
 
 def songInfo(request):
